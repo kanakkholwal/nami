812ea21961792fbd815730212bae9cbbb9cfb3ee
Kanak <kkupgrader.fs@gmail.com>
2024-03-09 23:07:52 +0530

    Guide Basic UI

--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -3,15 +3,23 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function UserDashboard() {
+
+
     return <div>
         <h1>User Dashboard</h1>
 
         <p>Here is the user dashboard</p>
 
-        <Link href="/user/settings">
-            <div className="flex gap-2">
-                <Button>Settings</Button>
-            </div>
+        <Button asChild>
+            <Link href="/user/find-places">
+                Find new Places
             </Link>
+        </Button>
+        <Button asChild>
+            <Link href="/user/find-trips">
+                Find new Trips
+            </Link>
+        </Button>
+
     </div>
 }
