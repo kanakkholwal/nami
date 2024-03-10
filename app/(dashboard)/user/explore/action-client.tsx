a19901e5ef4e0aa0a15dea100bad2bb8c413c64f
Kanak <kkupgrader.fs@gmail.com>
2024-03-10 10:18:48 +0530

    Add loading state to SearchBar component

--- app/(dashboard)/user/explore/action-client.tsx
+++ app/(dashboard)/user/explore/action-client.tsx
@@ -14,6 +14,7 @@ interface Props {
 
 
 export default function SearchBar({ getMedia }: Props) {
+    const [loading, setLoading] = React.useState(false);
     const [location, setLocation] = React.useState({
         lat: 0,
         long: 0
@@ -21,8 +22,14 @@ export default function SearchBar({ getMedia }: Props) {
     const [media, setMedia] = React.useState<any[]>([]);
     React.useEffect(() => {
         if (location.lat !== 0 && location.long !== 0) {
+            setLoading(true);
             getMedia(location).then((data) => {
                 setMedia(data);
+            }).catch((err:any) =>{
+                console.log(err);
+
+            }).finally(() => {
+                setLoading(false);
             })
         }
     }, [location, getMedia]);
@@ -32,6 +39,7 @@ export default function SearchBar({ getMedia }: Props) {
     return <div className="w-full flex flex-col gap-2">
 
         <Button 
+        disabled={loading}
         className="max-w-[10rem]"
         onClick={() => {
             navigator.geolocation.getCurrentPosition((position) => {
@@ -49,13 +57,17 @@ export default function SearchBar({ getMedia }: Props) {
             })
 
         }}>
-            Explore Now
+            {loading ? "Searching":"Explore Now"}
         </Button>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full">
             {media.map((item, index) => {
-                return <div key={index} className="flex flex-col gap-2">
+                return <div key={index} className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
                     <div>{item.title}</div>
-                    <img src={item.media_url} alt={item.title} />
+                    <img className="w-full h-56 object-cover object-center" src={item.media_url} alt={item.title} />
+                    <div className="p-4">
+                            <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{item.name}</h3>
+                        Uploaded on {new Date(item.createdAt).toDateString()}
+                    </div>
                 </div>
             })}
         </div>
